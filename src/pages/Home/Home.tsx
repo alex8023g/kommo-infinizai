494bca0113a368f7b276d75ffcda165944e8980c
alex8023 <alex8023@yandex.ru>
2023-08-20 15:09:48 +0300

    section-3 desktop finish

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import styles from './home.module.css';
 import { Header } from '../../components/Header';
 import { Layout } from '../../components/Layout';
 import { ReactComponent as Flame } from '../../img/flame.svg';
+import { ReactComponent as Underline } from '../../img/underline.svg';
 import { Button2 } from '../../components/Button2';
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
@@ -16,6 +17,11 @@ import imgChar1 from '../../img/character-1.png';
 import imgChar2 from '../../img/character-2.png';
 import imgChar3 from '../../img/character-3.png';
 import imgChar4 from '../../img/character-4.png';
+import imgGamepad from '../../img/gamepad.png';
+import imgGamer from '../../img/gamer.png';
+import imgPc from '../../img/pc.png';
+import imgKeyboard from '../../img/keyboard.png';
+import imgEva from '../../img/eva.png';
 import { nanoid } from 'nanoid';
 
 const tabData = [
@@ -44,7 +50,12 @@ export function Home() {
               <Flame />
               <span className={styles.futureSpan}>FUTURE OF eSPORTS</span>
             </div>
-            <h2 className={styles.s1h2}>Unleash the Next Generation of Gaming</h2>
+            <h2 className={styles.s1h2}>
+              Unleash the Next Generation of Gaming
+              <div className={styles.underline}>
+                <Underline />
+              </div>
+            </h2>
             <p className={styles.p1}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec donec in
               morbi pulvinar. Enim non pulvinar neque.
@@ -94,7 +105,6 @@ export function Home() {
                     className={cx(styles.tab, { [styles.activeTab]: i === activeChar })}
                     onClick={() => {
                       setActiveChar(i);
-                      console.log(i);
                     }}
                   >
                     <img src={src} alt='' />
@@ -105,9 +115,57 @@ export function Home() {
           </div>
         </Layout>
       </section>
-      <section>
-        <Layout>section-3</Layout>
+      <section className={styles.section3}>
+        <Layout>
+          <div className={styles.s3top}>
+            <div className={styles.s3topLeft}>
+              <div className={styles.futureWrap}>
+                <Flame />
+                <span className={styles.futureSpan}>FUTURE OF eSPORTS</span>
+              </div>
+              <h2 className={styles.s3h2}>Latest News & Articles</h2>
+            </div>
+            <Button2 name='Read More' />
+          </div>
+          <div className={styles.s3content}>
+            <div className={styles.gamepad}>
+              {/* <img src={imgGamepad} alt='' /> */}
+              <div className={styles.gamepadImg}></div>
+              <p className={styles.datev1}>April 2.2021</p>
+              <h3 className={styles.h3v1}>
+                Esports Group teams up with the Indianapolis Colts
+              </h3>
+            </div>
+            <div className={styles.gamer}>
+              {/* <img src={imgGamer} alt='' /> */}
+              <div className={styles.gamerImg}></div>
+              <p className={styles.datev2}>April 2.2021</p>
+              <h3 className={styles.h3v2}>NAVI reveals s1mple fifth anniversary</h3>
+            </div>
+            <div className={styles.pc}>
+              {/* <img src={imgPc} alt='' /> */}
+              <div className={styles.pcImg}></div>
+              <p className={styles.datev2}>April 2.2021</p>
+              <h3 className={styles.h3v2}>A1esports Shares new picture</h3>
+            </div>
+            <div className={styles.keyboard}>
+              {/* <img src={imgKeyboard} alt='' /> */}
+              <div className={styles.keyboardImg}></div>
+              <p className={styles.datev2}>April 2.2021</p>
+              <h3 className={styles.h3v2}>T1 unveil partnership with Razer</h3>
+            </div>
+            <div className={styles.eva}>
+              {/* <img src={imgEva} alt='' /> */}
+              <div className={styles.evaImg}></div>
+              <p className={styles.datev2}>April 2.2021</p>
+              <h3 className={styles.h3v2}>RX secures content partnership with</h3>
+            </div>
+          </div>
+        </Layout>
       </section>
+      <footer>
+        <Layout>Footer</Layout>
+      </footer>
     </>
   );
 }
